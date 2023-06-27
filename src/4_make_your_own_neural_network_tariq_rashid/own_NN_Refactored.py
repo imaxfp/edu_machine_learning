c446cc61131549ad8c17c4d798de4baefbc90cbf
imaxfp <imaxfp@gmail.com>
2023-06-27 15:35:09 +0300

    Make your own NN

--- src/4_make_your_own_neural_network_tariq_rashid/own_NN_Refactored.py
+++ src/4_make_your_own_neural_network_tariq_rashid/own_NN_Refactored.py
@@ -1,11 +1,10 @@
 import unittest
-import imageService as imgSrv
-import numpy
-import numpy as np
 from typing import List
 from collections import deque
+import numpy as np
 
 
+# You own Neural Network implementation
 class NeuronInputLayer:
     """
     In a typical neural network architecture, the input layer neurons do not have weights associated with them.
@@ -19,8 +18,8 @@ class NeuronInputLayer:
     This allows the network to assign different levels of significance to different input dimensions.
     """
 
-    def __init__(self, features_x: float = None):
-        self.features_x = features_x
+    def __init__(self, feature_x: float = None):
+        self.feature_x = feature_x
 
 
 class Neuron:
@@ -32,10 +31,7 @@ class Neuron:
     output.
     """
 
-    def __init__(self, weights: list, features_x=None, bias: float = 0.1):
-        if features_x is None:
-            features_x = []
-
+    def __init__(self, weights: list, features_x: list = None, bias: float = 0.1):
         self.weights = weights
         self.features_x = features_x
         self.bias = bias
@@ -43,12 +39,14 @@ class Neuron:
     def update_weights(self, weights: list):
         self.weights = weights
 
+    def update_features(self, x: list):
+        self.features_x = x
+
 
 class NeuralNetwork:
-
     # initialise the neural network
     def __init__(self):
-        self.layers = list()
+        self.layers = []
 
     def add_input_layer(self, features: List):
         if self.layers.__len__() == 0:
@@ -62,16 +60,18 @@ class NeuralNetwork:
             layers_count = len(self.layers)
             prev_layer_neurons = len(self.layers[layers_count - 1])
             # create new layer based on previous layer
-            hidden_layers_neurons = list()
-            for n in range(0, nodes):
+            hidden_layers_neurons = []
+            for _ in range(0, nodes):
                 # initialize weights with gaussian distribution
-                weights_gauss = self.link_weight_ih_and_ho_gaussian_distribution(prev_layer_neurons, nodes)
+                weights_gauss = self.link_weight_ih_and_ho_gaussian_distribution(
+                    prev_layer_neurons, nodes
+                )
                 hidden_layers_neurons.append(Neuron(weights_gauss))
 
             self.layers.append(hidden_layers_neurons)
 
     @staticmethod
-    def activation_function_sigmoid(x):
+    def activation_fun_sig(x):
         """
         Sigmoid activation function.
         :param x:
@@ -80,53 +80,49 @@ class NeuralNetwork:
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
-    def link_weight_ih_and_ho_gaussian_distribution(hidden_nodes: int, input_nodes: int):
-        """"
-        They sample the weights from a normal probability distribution centered around zero
+    def link_weight_ih_and_ho_gaussian_distribution(
+        hidden_nodes: int, input_nodes: int
+    ):
+        """_summary_
+                They sample the weights from a normal probability distribution centered around zero
         and with a standard deviation that is related to the number of incoming links into a node
         1/√(number of incoming links).
         In other words weights 'w' will be initialized with 67% probability in normal distribution window
 
         See https://www.techtarget.com/whatis/definition/normal-distribution
+
+        Args:
+            hidden_nodes (int): _description_
+            input_nodes (int): _description_
+
+        Returns:
+            _type_: _description_
         """
-        return numpy.random.normal(0.0, pow(hidden_nodes, -0.5), (hidden_nodes, input_nodes))
+        return np.random.normal(
+            0.0, pow(hidden_nodes, -0.5), (hidden_nodes, input_nodes)
+        )
 
-    # TODO old code
+    def forward_propagation(self):
+        """_summary_
+        Implementation of forward propagation algorithm
 
-    def link_weight_ih_and_ho_uniform_distribution(self):
+        Args:
+            inputs_list (_type_): _description_
+            targets_list (_type_): _description_
         """
-        Link weight matrices, weight 'w' for input and hidden, and 'w' for hidden and output
-        weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
-        w11 -> w21
-        w12 -> w22
-        etc ...
-        For simplicity, we’ll simply subtract 0.5 for range between − 0.5 to +0.5
-
-        See https://www.investopedia.com/terms/u/uniform-distribution.asp
-
-        :return:
-        """
-
-        self.w_input_hidden = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)
-        self.w_hidden_output = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)
-
-    def link_weight_ih_and_ho_gaussian_distribution(self):
-        """"
-        They sample the weights from a normal probability distribution centered around zero
-        and with a standard deviation that is related to the number of incoming links into a node
-        1/√(number of incoming links).
-        In other words weights 'w' will be initialized with 67% probability in normal distribution window
-
-        See https://www.techtarget.com/whatis/definition/normal-distribution
-        """
-
-        self.w_input_hidden = numpy.random.normal(0.0,
-                                                  pow(self.hidden_nodes, -0.5),
-                                                  (self.hidden_nodes, self.input_nodes))
-
-        self.w_hidden_output = numpy.random.normal(0.0,
-                                                   pow(self.output_nodes, -0.5),
-                                                   (self.output_nodes, self.hidden_nodes))
+        input_layer_features = [x.feature_x for x in self.layers[0]]
+        layer_output = []
+        for layer in self.layers[1:]:
+            for neurone in layer:
+                neurone.features_x = input_layer_features
+                n_weights = np.array(neurone.weights, ndmin=2).T
+                n_features = np.array(neurone.features_x, ndmin=2).T
+                # Matrices multiplication see http
+                neuron_output = self.activation_fun_sig(np.dot(n_features, n_weights))
+                layer_output.append(neuron_output)
+            input_layer_features = layer_output.copy()
+            layer_output.clear()    
+                
 
     def train(self, inputs_list, targets_list):
         """
@@ -144,12 +140,12 @@ class NeuralNetwork:
         # calculate signals into hidden layer. If both `a` and `b` are 2-D arrays, it is matrix multiplication
         hidden_inputs = numpy.dot(self.w_input_hidden, inputs)
         # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function_sigmoid(hidden_inputs)
+        hidden_outputs = self.activation_fun_sig(hidden_inputs)
 
         # calculate signals into final output layer
         final_inputs = numpy.dot(self.w_hidden_output, hidden_outputs)
         # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function_sigmoid(final_inputs)
+        final_outputs = self.activation_fun_sig(final_inputs)
 
         # output layer error is the (target - actual)
         # TODO play around additional loss functions (MSE, etc)
@@ -161,12 +157,16 @@ class NeuralNetwork:
 
         # TODO do not repeat yourself, use single function - "backpropagation_algorithm"
         # update the weights for the links between the hidden and output layers
-        self.w_hidden_output += self.learning_grate * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
-                                                                numpy.transpose(hidden_outputs))
+        self.w_hidden_output += self.learning_grate * numpy.dot(
+            (output_errors * final_outputs * (1.0 - final_outputs)),
+            numpy.transpose(hidden_outputs),
+        )
 
         # update the weights for the links between the input and hidden layers
         self.w_input_hidden += self.learning_grate * numpy.dot(
-            (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+            (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+            numpy.transpose(inputs),
+        )
 
         pass
 
@@ -183,22 +183,42 @@ class NeuralNetwork:
         # calculate signals into hidden layer
         hidden_inputs = numpy.dot(self.w_input_hidden, inputs)
         # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function_sigmoid(hidden_inputs)
+        hidden_outputs = self.activation_fun_sig(hidden_inputs)
         # TODO query_layer "do not repit yourself"
         # calculate signals into final output layer
         final_inputs = numpy.dot(self.w_hidden_output, hidden_outputs)
         # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function_sigmoid(final_inputs)
+        final_outputs = self.activation_fun_sig(final_inputs)
 
         return final_outputs
 
 
-class NeuralNetworkTest(unittest.TestCase):
+# TESTS
+
+
+class NeuralNetworkBasicTest(unittest.TestCase):
     def setUp(self):
-        # Create an instance of the Calculator class for each test
         self.nn = NeuralNetwork()
 
     def test_add_input_layer(self):
-        features = [1, 2, 3]
-        self.nn.add_input_layer(features)
-        self.assertEqual(len(self.nn.layers), 1)
+        self.nn.add_input_layer(features=[1, 21, 31])
+        self.assertEqual(len(self.nn.layers[0]), 3)
+
+    def test_add_layer(self):
+        self.nn.add_input_layer(features=[1, 0, 3])
+        self.nn.add_layer(nodes=10)
+        # check amount of neurons for the layer 1
+        self.assertEqual(len(self.nn.layers[1]), 10)
+        # check amount of weights for the neuron of layer 1. Previous layer - 0 layer, has 3 feature.
+        neuron_first_layer = self.nn.layers[1][0]
+        self.assertEqual(len(neuron_first_layer.weights), 3)
+
+    def test_forward_propagation(self):
+        self.nn.add_input_layer(features=[1, 2, 3])
+        self.nn.add_layer(3)
+        self.nn.forward_propagation()
+
+
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
